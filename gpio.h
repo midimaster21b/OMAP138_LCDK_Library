5ad95cca649396ea08be5dd3fdc87b7ed7594407
Joshua Edgcombe <joshedgcombe@gmail.com>
2017-12-06 23:48:54 -0500

    Fix GPIO base memory address issue

--- gpio.h
+++ gpio.h
@@ -8,7 +8,7 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
-#define GPIO_MEMORY_BASE (*(volatile uint32_t*)0x01E26000)
+#define GPIO_MEMORY_BASE 0x01E26000
 
 // GPIO0ARM_DIR67
 // 0x01E26088
@@ -22,6 +22,17 @@
 #define GPIO_SET_FAL_TRIG67 (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xA4))
 #define GPIO_CLR_FAL_TRIG67 (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xA8))
 #define GPIO_INTSTAT67      (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xAC))
+
+#define GPIO_DIR8           (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xB0))
+#define GPIO_OUT_DATA8      (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xB4))
+#define GPIO_SET_DATA8      (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xB8))
+#define GPIO_CLR_DATA8      (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xBC))
+#define GPIO_IN_DATA8       (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xC0))
+#define GPIO_SET_RIS_TRIG8  (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xC4))
+#define GPIO_CLR_RIS_TRIG8  (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xC8))
+#define GPIO_SET_FAL_TRIG8  (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xCC))
+#define GPIO_CLR_FAL_TRIG8  (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xD0))
+#define GPIO_INTSTAT8       (*(volatile uint32_t*)(GPIO_MEMORY_BASE + 0xD4))
 
 #define GPIO_PORT_7_PINS    0xFFFF0000
 
