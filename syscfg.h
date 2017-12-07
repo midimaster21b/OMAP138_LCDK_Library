292d5979a49ba5b9b5c7f2402cea16b228f762ee
Joshua Edgcombe <joshedgcombe@gmail.com>
2017-12-07 01:03:41 -0500

    Write initial syscfg header

---
+++ syscfg.h
@@ -0,0 +1,33 @@
+/*
+ * syscfg.h
+ *
+ *  Created on: Dec 7, 2017
+ *      Author: Joshua Edgcombe
+ */
+
+#ifndef SYSCFG_H_
+#define SYSCFG_H_
+
+#define SYSCFG0ARM_PINMUX0    (*(volatile uint32_t*)(0x01C14120))
+#define SYSCFG0ARM_PINMUX1    (*(volatile uint32_t*)(0x01C14124))
+#define SYSCFG0ARM_PINMUX2    (*(volatile uint32_t*)(0x01C14128))
+#define SYSCFG0ARM_PINMUX3    (*(volatile uint32_t*)(0x01C1412C))
+#define SYSCFG0ARM_PINMUX4    (*(volatile uint32_t*)(0x01C14130))
+#define SYSCFG0ARM_PINMUX5    (*(volatile uint32_t*)(0x01C14134))
+#define SYSCFG0ARM_PINMUX6    (*(volatile uint32_t*)(0x01C14138))
+#define SYSCFG0ARM_PINMUX7    (*(volatile uint32_t*)(0x01C1413C))
+#define SYSCFG0ARM_PINMUX8    (*(volatile uint32_t*)(0x01C14140))
+#define SYSCFG0ARM_PINMUX9    (*(volatile uint32_t*)(0x01C14144))
+#define SYSCFG0ARM_PINMUX10   (*(volatile uint32_t*)(0x01C14148))
+#define SYSCFG0ARM_PINMUX11   (*(volatile uint32_t*)(0x01C1414C))
+#define SYSCFG0ARM_PINMUX12   (*(volatile uint32_t*)(0x01C14150))
+#define SYSCFG0ARM_PINMUX13   (*(volatile uint32_t*)(0x01C14154))
+#define SYSCFG0ARM_PINMUX14   (*(volatile uint32_t*)(0x01C14158))
+#define SYSCFG0ARM_PINMUX15   (*(volatile uint32_t*)(0x01C1415C))
+#define SYSCFG0ARM_PINMUX16   (*(volatile uint32_t*)(0x01C14160))
+#define SYSCFG0ARM_PINMUX17   (*(volatile uint32_t*)(0x01C14164))
+#define SYSCFG0ARM_PINMUX18   (*(volatile uint32_t*)(0x01C14168))
+#define SYSCFG0ARM_PINMUX19   (*(volatile uint32_t*)(0x01C1416C))
+
+
+#endif /* SYSCFG_H_ */
